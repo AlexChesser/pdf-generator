d6988876c74522a05746be903df34dba9fb667dc
Alex Chesser <achesser@klick.com>
2017-06-07 15:58:37 -0400

    progress on PDF FILE saving.

--- src/android/PDFPrinterWebView.java
+++ src/android/PDFPrinterWebView.java
@@ -15,10 +15,12 @@ import android.webkit.WebViewClient;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.LOG;
 
+import android.util.Base64;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.lang.System;
-
 
 import android.print.PdfPrint;
 import android.print.PrintAttributes;
@@ -65,13 +67,33 @@ public class PDFPrinterWebView extends WebViewClient {
                 .setMediaSize(PrintAttributes.MediaSize.ISO_A4)
                 .setResolution(new PrintAttributes.Resolution("pdf", "pdf", 600, 600))
                 .setMinMargins(PrintAttributes.Margins.NO_MARGINS).build();
-        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM + "/PDFTest/");
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
         PdfPrint pdfPrint = new PdfPrint(attributes);
-        pdfPrint.print(webView.createPrintDocumentAdapter(jobName), path, "output_" + System.currentTimeMillis() + ".pdf");
+        String temp_filename = "output_" + System.currentTimeMillis() + ".pdf";
 
+        // permissions required in ANDROID.MANIFEST
+        // this is a temporary measure in order to get the PDF saved to disk
+        // after this is done we'll try to go straight to a ByteArrayOutputStream
+        // (or something that allows in-memory only file saving)
+        //    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+        //    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
+        pdfPrint.print(webView.createPrintDocumentAdapter(jobName), path, temp_filename);
 
+        String encodedBase64 = null;
+        try {
+            File printedFile = new File(path, temp_filename);
+            FileInputStream fileInputStreamReader = new FileInputStream(printedFile);
+            byte[] bytes = new byte[(int)printedFile.length()];
+            fileInputStreamReader.read(bytes);
 
-        this.cordovaCallback.success("success");
+            encodedBase64 = Base64.encodeToString( bytes, Base64.DEFAULT );
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.cordovaCallback.success(encodedBase64);
     }
 
 }
